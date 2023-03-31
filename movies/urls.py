b2650d6717e2b5a8ff84a5fa498c7541e5e1f0c5
Dylan Poffenberger <dylpoff2001@gmail.com>
2023-03-31 00:13:06 -0400

    add form for adding movie

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name = "index")
+    path('', views.index, name = "index"),
+    path('add_movie/', views.add_movie, name = "add-movie")
 ]
